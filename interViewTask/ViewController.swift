1241ffda910293ef22e16357952bf578330a9861
challaCBN123 <challabhargava55@gmail.com>
2020-08-14 21:06:25 +0530

    Initial Commit

---
+++ interViewTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  interViewTask
+//
+//  Created by Bhargava on 14/08/20.
+//  Copyright © 2020 Bhargava. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
